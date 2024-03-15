5d7650a3f0fc53013a2dff8f0ee226d51b820f56
RodrigoTheDev <rocapucho123123@gmail.com>
2024-03-15 10:16:07 -0300

    fix: removido formulário de ponto comercial

--- public_html/public/js/cadastro-abas.js
+++ public_html/public/js/cadastro-abas.js
@@ -1,18 +1,18 @@
 const abas = [
     document.getElementById("adicionar-casa-container"),
-    document.getElementById("adicionar-apartamento-container"),
+    // document.getElementById("adicionar-apartamento-container"),
     document.getElementById("adicionar-terreno-container")
 ]
 
 const abas_buttons = [
     document.getElementById("btn-aba-casa"),
-    document.getElementById("btn-aba-apartamento"),
+    // document.getElementById("btn-aba-apartamento"),
     document.getElementById("btn-aba-terreno")
 ]
 
 //por padrão, a casa vem ativada
 abas[1].style = "display:none";
-abas[2].style = "display:none";
+// abas[2].style = "display:none";
 abas_buttons[0].classList.add("aba-option-selected");
 
 function openCasa() {
@@ -37,19 +37,19 @@ function openTerreno() {
     resetDisplay();
     resetClass();
 
-    abas[2].style = "display:flex";
+    abas[1].style = "display:flex";
 
-    abas_buttons[2].classList.add("aba-option-selected");
+    abas_buttons[1].classList.add("aba-option-selected");
 }
 
 function resetDisplay() {
     abas[0].style = "display:none";
     abas[1].style = "display:none";
-    abas[2].style = "display:none";
+    // abas[2].style = "display:none";
 }
 
 function resetClass() {
     abas_buttons[0].classList.remove("aba-option-selected")
     abas_buttons[1].classList.remove("aba-option-selected")
-    abas_buttons[2].classList.remove("aba-option-selected")
+    // abas_buttons[2].classList.remove("aba-option-selected")
 }
