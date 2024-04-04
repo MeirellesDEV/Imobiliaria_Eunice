1a07b50f3fbc5271164483a80ed617fcb79b0af8
RodrigoTheDev <rocapucho123123@gmail.com>
2024-04-04 09:34:10 -0300

    feat: página de erro 404

--- public_html/routes/web.php
+++ public_html/routes/web.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\clienteController;
 
 Route::get('/404', function(){abort(404);});
 
+// Route::get('/', [masterController::class, 'index']);
 Route::get('/', [masterController::class, 'index']);
 Route::post('/', [masterController::class, 'store']);
 
